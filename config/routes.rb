1d806cdc63a369afa41a2c6e0107544924c991b5
nohaderf <freda.hon@gmail.com>
2020-11-05 13:36:48 -0500

    commit tree 8caa122039e79e75031285ecf7b0b6a6079a9064

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'products/index', to: 'products#index', as: 'products'
+  # post 'products/', to: 'products#add', as: 'add_product'
+
+  root 'products#index'
+  post '/' => 'products#add'
 end
